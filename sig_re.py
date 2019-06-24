7fb62f4fe91cea7c68369040655adfaa458bedc0
vbox.fred21 <393845821@qq.com>
2019-06-24 22:56:57 +0800

    fred21 update 2

--- sig_re.py
+++ sig_re.py
@@ -1,4 +1,4 @@
-66import os    
+import os    
 import signal    
 from time import sleep  
 
@@ -11,10 +11,10 @@ from time import sleep
 #comment : add by fred21 2019-6-23
 #comment : add by fred21 2019-6-24
 #comment : add by fred21 2019-6-24_1
-#comment : add by fred21 2019-6-24_2
+#comment : add by hwfred21 2019-6-24
 #comment : add by fred21 2019-6-25
-
-def onsignal_term(a,b):    
+#comment : add by fred21 2019-6-24_1def onsignal_term(a,b):    
+def onsignal_term(a,b):    	print 'get SIGTERM'    
 	print 'get SIGTERM'    
      
 signal.signal(signal.SIGTERM,onsignal_term)    
